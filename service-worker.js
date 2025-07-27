c5a1a05270c0c3ed36cd932ceab7e1092b111c29
elvisfreitas317 <elvis_kennedy@hotmail.com>
2025-07-27 10:48:57 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("lista-compras-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/service-worker.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
